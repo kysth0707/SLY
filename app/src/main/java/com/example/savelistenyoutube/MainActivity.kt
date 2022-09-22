a0576f2e76378edd17e158f2ef661520e0547fee
kysth0707 <kysth0707@naver.com>
2022-09-22 21:02:46 +0900

    feat : added button and edit text
    Now I can use it

--- app/src/main/java/com/example/savelistenyoutube/MainActivity.kt
+++ app/src/main/java/com/example/savelistenyoutube/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.savelistenyoutube
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.widget.BaseAdapter
+import java.security.AccessControlContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ButtonEvent = findViewById<Button>(R.id.search_button)
+        val YoutubeLinkText = findViewById<EditText>(R.id.youtube_link_text)
+        val ListViews = findViewById<ListView>(R.id.list_view)
+//        ListViews.app
+
+        ButtonEvent.setOnClickListener {
+            Log.v("0", YoutubeLinkText.text.toString())
+        }
     }
 }
